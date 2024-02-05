99e392133084a0bc278d4f4d919ee1d1c2c398b5
Mohammad sadique <mohammadsadique110786@gmail.com>
2024-02-05 12:56:59 +0530

    Just playing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
       <div className="w-full h-screen flex flex-col justify-center items-center bg-black/80 text-white">
         <h1 className="text-5xl my-3">Shopenture</h1>
         <h2 className="text-lg">Where every buy is an adventure</h2>
+        <h2>Sadique is here</h2>
       </div>
     </>
   );
